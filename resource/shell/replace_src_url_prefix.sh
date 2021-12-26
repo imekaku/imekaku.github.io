ea45faaa9122ebba35fa3a0c08904ee0545c782e
imekaku <imekaku@gmail.com>
2021-12-27 02:16:01 +0800

    add new reading

--- resource/shell/replace_src_url_prefix.sh
+++ resource/shell/replace_src_url_prefix.sh
@@ -3,4 +3,7 @@
 find . -name "*.md" -type f | xargs sed -i '' 's/https:\/\/raw.githubusercontent.com\/imekaku\/MyPicture\/master\/github-blog-pic/http:\/\/blogcdn.qihope.com\/github-blog-pic/g'
 
 # 替换博客thinking中的图片前缀为国内域名地址
-find . -name "*.md" -type f | xargs sed -i '' 's/https:\/\/raw.githubusercontent.com\/imekaku\/MyPicture\/master\/github-thinking-pic/http:\/\/blogcdn.qihope.com\/github-blog-pic/g'
+find . -name "*.md" -type f | xargs sed -i '' 's/https:\/\/raw.githubusercontent.com\/imekaku\/MyPicture\/master\/github-thinking-pic/http:\/\/blogcdn.qihope.com\/github-thinking-pic/g'
+
+# 替换博客reading中的图片前缀为国内域名地址
+find . -name "*.md" -type f | xargs sed -i '' 's/https:\/\/raw.githubusercontent.com\/imekaku\/MyPicture\/master\/github-reading-pic/https:\/\/blogcdn.qihope.com\/github-reading-pic/g'
