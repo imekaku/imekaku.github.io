256f99f596694ff1cd5cc7854410ed3a6f0b6850
imekaku <imekaku@gmail.com>
2023-01-05 21:59:14 +0800

    add new tool shell

--- resource/shell/replace_src_url_prefix.sh
+++ resource/shell/replace_src_url_prefix.sh
@@ -7,3 +7,7 @@ find . -name "*.md" -type f | xargs sed -i '' 's/https:\/\/raw.githubusercontent
 
 # 替换博客reading中的图片前缀为国内域名地址
 find . -name "*.md" -type f | xargs sed -i '' 's/https:\/\/raw.githubusercontent.com\/imekaku\/MyPicture\/master\/github-reading-pic/https:\/\/blogcdn.qihope.com\/github-reading-pic/g'
+
+# 替换http为https
+find . -name "*.md" -type f | xargs sed -i '' 's/http:\/\/blogcdn.qihope.com\/github-thinking-pic/https:\/\/blogcdn.qihope.com\/github-thinking-pic/g'
+find . -name "*.md" -type f | xargs sed -i '' 's/http:\/\/blogcdn.qihope.com\/github-blog-pic/https:\/\/blogcdn.qihope.com\/github-blog-pic/g'
